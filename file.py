9ee1fb2d1ca3eeac1960ec86beb60ab75240e774
Benjamin <bstavare@calpoly.edu>
2024-11-18 21:56:09 -0800

    completed lab 8

--- file.py
+++ file.py
@@ -1,11 +1,24 @@
 import sys
 
+
 file = sys.argv[1]
 
 try:
     with open(file, 'r') as open_file:
-        for line in file:
-            pass
+        for line in open_file:
+            split_line = line.strip().split(',')
+            if len(split_line) != 2:
+                print(f'error: invalid format: "{line.strip()}"')
+                continue
+
+            try:
+                float_1 = float(split_line[0].strip())
+                float_2 = float(split_line[1].strip())
+                print(float_1 + float_2)
+            except ValueError as ve:
+                print(f'error: invalid line "{line.strip()}": {ve}')
+                continue
 
 except FileNotFoundError as fnfe:
     print(f'error: file "{file}" not found')
+
